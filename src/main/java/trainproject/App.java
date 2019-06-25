2e2120f676cf30b6a02f56911d1c55edfee1c9e8
pircklr1 <rico.pircklen@gmail.com>
2019-06-25 14:10:51 +0300

    päivitys

--- src/main/java/trainproject/App.java
+++ src/main/java/trainproject/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Sannan testi" );
+        System.out.println( "Sannandsdsdsd testi" );
 
     }
 }
