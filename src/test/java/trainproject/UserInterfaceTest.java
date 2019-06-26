0993dd04ce6bc3b8b0feb65e371f38352891465d
pircklr1 <rico.pircklen@gmail.com>
2019-06-26 11:34:26 +0300

    test filet

---
+++ src/test/java/trainproject/UserInterfaceTest.java
@@ -0,0 +1,15 @@
+package trainproject;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class UserInterfaceTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
