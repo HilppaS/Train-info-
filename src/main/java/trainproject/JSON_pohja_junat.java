33b1e651540e2d5a3fe7a540edefedbbc2521b89
pircklr1 <rico.pircklen@gmail.com>
2019-06-26 19:33:49 +0300

    hashmappi

--- src/main/java/trainproject/JSON_pohja_junat.java
+++ src/main/java/trainproject/JSON_pohja_junat.java
@@ -20,7 +20,7 @@ Asentuu Jacksonin databind, sekä core ja annotations
 
 public class JSON_pohja_junat {
     public static void main(String[] args) {
-        lueJunanJSONData();
+        createListOfTrainStations();
     }
 
     private static void lueJunanJSONData() {
@@ -28,7 +28,7 @@ public class JSON_pohja_junat {
         String baseurl = "https://rata.digitraffic.fi/api/v1";
         try {
             // Määritetään url-parametriin haettava asia (esim. live-junat, Helsingistä Lahteen)
-            URL url = new URL(URI.create(String.format("%s/live-trains/station/hki/LH", baseurl)).toASCIIString());
+            URL url = new URL(URI.create(String.format("%s/live-trains/station/HKI/LH", baseurl)).toASCIIString());
             ObjectMapper mapper = new ObjectMapper();
             CollectionType tarkempiListanTyyppi = mapper.getTypeFactory().constructCollectionType(ArrayList.class, Juna.class);
             List<Juna> junat = mapper.readValue(url, tarkempiListanTyyppi);  // pelkkä List.class ei riitä tyypiksi
@@ -41,6 +41,59 @@ public class JSON_pohja_junat {
             System.out.println("Eka juna lähtee ekalta asemalta: " + junat.get(0).getTimeTableRows().get(0).getScheduledTime());
             //   i++;
             // }
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    private static void kahdenAsemanValillaLiikkeessaOlevatJunat() {
+        // Määritetään API:n osoite, mistä JSON-datat haetaan
+        String baseurl = "https://rata.digitraffic.fi/api/v1";
+        try {
+            // Määritetään url-parametriin haettava asia (esim. live-junat, Helsingistä Lahteen)
+            URL url = new URL(URI.create(String.format("%s/live-trains/station/HKI/OL", baseurl)).toASCIIString());
+            ObjectMapper mapper = new ObjectMapper();
+            CollectionType tarkempiListanTyyppi = mapper.getTypeFactory().constructCollectionType(ArrayList.class, Juna.class);
+            List<Juna> junat = mapper.readValue(url, tarkempiListanTyyppi);  // pelkkä List.class ei riitä tyypiksi
+            int i = 0;
+            Date lahto = junat.get(0).getTimeTableRows().get(2).getScheduledTime();
+            Date saapuminen = junat.get(0).getTimeTableRows().get(4).getScheduledTime();
+            long ero = saapuminen.getTime() - lahto.getTime();
+            System.out.println("Erotus " + ero);
+            for (Juna j : junat) {
+                System.out.println(j.isRunningCurrently());
+                System.out.println("Juna on lähtenyt asemaltaan: " + junat.get(i).getTimeTableRows().get(0).getScheduledTime());
+              /*  if(j.isRunningCurrently()) {
+                    System.out.println("Junan numero on " + junat.get(i).getTrainNumber());
+                    System.out.println("Junan lähtöpäivä on " + junat.get(i).getDepartureDate());
+                    System.out.println("Juna on lähtenyt asemaltaan: " + junat.get(0).getTimeTableRows().get(0).getScheduledTime());
+                    i++;
+                }*/
+                i++;
+            }
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    private static void createListOfTrainStations() {
+        // Määritetään API:n osoite, mistä JSON-datat haetaan
+        String baseurl = "https://rata.digitraffic.fi/api/v1";
+        try {
+            // Määritetään url-parametriin haettava asia (esim. live-junat, Helsingistä Lahteen)
+            URL url = new URL(URI.create(String.format("%s/metadata/stations", baseurl)).toASCIIString());
+            ObjectMapper mapper = new ObjectMapper();
+
+            CollectionType tarkempiListanTyyppi = mapper.getTypeFactory().constructCollectionType(ArrayList.class, TrainStations.class);
+            //Map<String, Object> mapObject = new HashMap<String, Object>();
+
+            //List<Juna> junat = mapper.readValue(url, tarkempiListanTyyppi);  // pelkkä List.class ei riitä tyypiksi
+            List<TrainStations> asemat = mapper.readValue(url, tarkempiListanTyyppi);
+            //int i = 0;
+            System.out.println(asemat.get(0).getStationName());
+            //System.out.println("Asemat: " + asemat.get(0));
 
         } catch (Exception ex) {
             System.out.println(ex);
---
+++ src/main/java/trainproject/TrainStations.java
@@ -0,0 +1,69 @@
+package trainproject;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TrainStations {
+    public static void main(String[] args) {
+        createListOfTrainStations();
+    }
+
+    boolean passengerTraffic;
+    String type;
+    String stationName;
+    String stationShortCode;
+    int stationUICCode;
+    String countryCode;
+    double longitude;
+    double latitude;
+
+    public String getStationName() {
+        return stationName;
+    }
+
+    public String getStationShortCode() {
+        return stationShortCode;
+    }
+
+    private static void createListOfTrainStations() {
+        // Määritetään API:n osoite, mistä JSON-datat haetaan
+        String baseurl = "https://rata.digitraffic.fi/api/v1";
+        try {
+            // Määritetään url-parametriin haettava asia (esim. live-junat, Helsingistä Lahteen)
+            URL url = new URL(URI.create(String.format("%s/metadata/stations", baseurl)).toASCIIString());
+            ObjectMapper mapper = new ObjectMapper();
+
+            CollectionType tarkempiListanTyyppi = mapper.getTypeFactory().constructCollectionType(ArrayList.class, TrainStations.class);
+
+            //List<Juna> junat = mapper.readValue(url, tarkempiListanTyyppi);  // pelkkä List.class ei riitä tyypiksi
+            List<TrainStations> asemat = mapper.readValue(url, tarkempiListanTyyppi);
+            //int i = 0;
+            //System.out.println(asemat.get(0).getStationName());
+            //System.out.println("Asemat: " + asemat.get(0));
+
+            Map<String, Object> asemaTaulukko = new HashMap<String, Object>();
+
+            for(TrainStations asema : asemat) {
+                asemaTaulukko.put(asema.getStationName(), asema.getStationShortCode());
+            }
+
+            // Lyhenne on value (containsValue), kaupungin nimi on key (containsKey)
+            System.out.println(asemaTaulukko);
+            System.out.println(asemaTaulukko.containsKey("Nastola"));
+            System.out.println(asemaTaulukko.containsValue("HKI"));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+        // }
+    }
+}
