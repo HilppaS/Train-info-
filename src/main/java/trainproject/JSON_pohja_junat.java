987a98329a7803d431c0edb2aa01f90f88aa80c6
Sanna <sanna.hilpas@gmail.com>
2019-06-27 09:57:30 +0300

    Masterin copypaste...

--- src/main/java/trainproject/JSON_pohja_junat.java
+++ src/main/java/trainproject/JSON_pohja_junat.java
@@ -23,10 +23,8 @@ Asentuu Jacksonin databind, sekä core ja annotations
 
 public class JSON_pohja_junat {
 
-
-     public static void main(String[] args) {
-         kahdenAsemanValillaLiikkeessaOlevatJunat();
-
+    public static void main(String[] args) {
+        kahdenAsemanValillaLiikkeessaOlevatJunat();
     }
 
     public static void lueJunanJSONData() {
@@ -47,7 +45,6 @@ public class JSON_pohja_junat {
             System.out.println("TimetableRow ekalle junalle: " + junat.get(i).getTimeTableRows());
             System.out.println("Juna lähtee asemalta: " + junat.get(0).getTimeTableRows().get(0).getScheduledTime());
             System.out.println("");
-
             //   i++;
             // }
         } catch (Exception ex) {
@@ -55,8 +52,47 @@ public class JSON_pohja_junat {
         }
     }
 
-      // TUOMAS : Seuraavan junan lähtö
-      public static void tulostaSeuraavaJunaLähtöJaMääräasemienPerusteella(String departureStation, String arrivalStation) {
+    ///SANNAN KOODIA... TESTIT: MM. PYSÄHTYMINEN, TOIMIIKO PYSÄHTYMISAIKA
+
+    public static void printTrainInfo(String trainnumber) {
+        // Määritetään API:n osoite, mistä JSON-datat haetaan
+        String baseurl = "https://rata.digitraffic.fi/api/v1";
+
+        try {
+            // Määritetään url-parametriin haettava asia (esim. live-junat tietyllä numerolla)
+            URL url = new URL(URI.create(String.format("%s/trains/2019-06-27/"+ trainnumber, baseurl)).toASCIIString());
+            ObjectMapper mapper = new ObjectMapper();
+            CollectionType tarkempiListanTyyppi = mapper.getTypeFactory().constructCollectionType(ArrayList.class, Juna.class);
+            List<Juna> junat = mapper.readValue(url, tarkempiListanTyyppi);  // pelkkä List.class ei riitä tyypiksi
+            int i = 0;
+            //  for (Juna j : junat) {
+            System.out.println("Junan numero on " + junat.get(i).getTrainNumber());
+            System.out.println("Junan lähtöpäivä on " + junat.get(i).getDepartureDate());
+            System.out.println("lähtöasema: " + junat.get(0).getTimeTableRows().get(0).getStationShortCode());
+            for ( int b=1; b<junat.get(0).getTimeTableRows().size()-1; b=b+2) {
+                Date saapuminen =junat.get(0).getTimeTableRows().get(b+1).getScheduledTime();
+                Date lahto = junat.get(0).getTimeTableRows().get(b).getScheduledTime();
+                long erotus =(saapuminen.getTime() - lahto.getTime());
+                erotus = erotus/60000;
+            /*    String hms = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(erotus),
+                        TimeUnit.MILLISECONDS.toMinutes(erotus) % TimeUnit.HOURS.toMinutes(1),
+                        TimeUnit.MILLISECONDS.toSeconds(erotus) % TimeUnit.MINUTES.toSeconds(1));
+             */   if ( junat.get(0).getTimeTableRows().get(b).trainStopping){
+                    System.out.println("asema: " + junat.get(0).getTimeTableRows().get(b).getStationShortCode() + " pysähtymisaika: " + erotus +" min");
+                }
+            }
+            System.out.println("pääteasema: " + junat.get(0).getTimeTableRows().get(junat.get(0).getTimeTableRows().size()-1).getStationShortCode());
+
+            System.out.println("Juna lähtee ekalta asemalta: " + junat.get(0).getTimeTableRows().get(0).getScheduledTime());
+            //   i++;
+            // }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+
+    public static void tulostaSeuraavaJunaLähtöJaMääräasemienPerusteella(String departureStation, String arrivalStation) {
         String baseurl = "https://rata.digitraffic.fi/api/v1";
 
         try {
@@ -75,9 +111,8 @@ public class JSON_pohja_junat {
             System.out.println(ex);
         }
     }
-  
-    // TUOMAS
-      public static void returnDepartedTrainsFromStation(String departureStation) {
+
+    public static void returnDepartedTrainsFromStation(String departureStation) {
         String baseurl = "https://rata.digitraffic.fi/api/v1";
 
         try {
@@ -86,34 +121,6 @@ public class JSON_pohja_junat {
             CollectionType tarkempiListanTyyppi = mapper.getTypeFactory().constructCollectionType(ArrayList.class, Juna.class);
             List<Juna> junat = mapper.readValue(url, tarkempiListanTyyppi);  // pelkkä List.class ei riitä tyypiksi
             int i = 0;
-
-            // for (Juna j : junat) {
-            if (junat.get(0).runningCurrently) {
-                System.out.println("The latest train departed from " + departureStation + " is: Train " + junat.get(0).getTrainNumber());
-            // Tähän lisättävä "junat.get(0).get.Timetablerows(0) stationShortCode" eli mille asemalle juna saapuu seuraavaksi + get.ScheduledTime
-            } else {
-                System.out.println("Train not running currently");
-            }
-            } catch(Exception ex){
-            System.out.println(ex);
-        }
-    }
-
-        ///SANNAN KOODIA... TESTIT: MM. PYSÄHTYMINEN, TOIMIIKO PYSÄHTYMISAIKA; HAKU JUNAN NUMERON PERUSTEELLA
-
-    public static void printTrainInfo(String trainnumber) {
-        // Määritetään API:n osoite, mistä JSON-datat haetaan
-        String baseurl = "https://rata.digitraffic.fi/api/v1";
-
-        try {
-            // Määritetään url-parametriin haettava asia (esim. live-junat tietyllä numerolla)
-            URL url = new URL(URI.create(String.format("%s/trains/2019-06-27/"+ trainnumber, baseurl)).toASCIIString());
-
-            ObjectMapper mapper = new ObjectMapper();
-            CollectionType tarkempiListanTyyppi = mapper.getTypeFactory().constructCollectionType(ArrayList.class, Juna.class);
-            List<Juna> junat = mapper.readValue(url, tarkempiListanTyyppi);  // pelkkä List.class ei riitä tyypiksi
-            int i = 0;
-
 
             // for (Juna j : junat) {
             if (junat.get(0).runningCurrently) {
@@ -127,8 +134,7 @@ public class JSON_pohja_junat {
         }
     }
 
-      // TUOMAS: EI KÄYTÖSSÄ
-   public static void tulostaHyvaksyttavatAsemat() {
+    public static void tulostaHyvaksyttavatAsemat() {
         String baseurl = "https://rata.digitraffic.fi/api/v1";
         try {
             URL url = new URL(URI.create(String.format("%s/live-trains/station/hki/LH", baseurl)).toASCIIString());
@@ -145,8 +151,8 @@ public class JSON_pohja_junat {
         } catch (Exception ex) {
             System.out.println(ex);
         }
+    }
 
-        // RICO: LIIKKEELLÄ OLEVAT JUNAT
     private static void kahdenAsemanValillaLiikkeessaOlevatJunat() {
         // Määritetään API:n osoite, mistä JSON-datat haetaan
         String baseurl = "https://rata.digitraffic.fi/api/v1";
@@ -168,13 +174,9 @@ public class JSON_pohja_junat {
                     System.out.println("Junan lähtöpäivä on " + j.getDepartureDate());
                     System.out.println("Juna on lähtenyt asemaltaan: " + j.getTimeTableRows().get(0).getScheduledTime());
                 }
-            
+            }
         } catch (Exception ex) {
             System.out.println(ex);
         }
     }
-
-
-
 }
-
